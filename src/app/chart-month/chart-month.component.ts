03e50224befbd4516922765f16466c4e1928594c
KamLan <adouardcamille@hotmail.com>
2018-06-26 14:39:44 +0200

    test travis

--- src/app/chart-month/chart-month.component.ts
+++ src/app/chart-month/chart-month.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { JQuery } from 'jquery';
 
 @Component({
   selector: 'app-chart-month',
@@ -39,7 +40,35 @@ export class ChartMonthComponent implements OnInit {
   }
 
   calculus(){
-
+    
+      JQuery('.chart').segbar([
+        {
+            data: [
+                {  value: 16744 },
+                {  value: 6500 },
+                {  value: 32750 },
+                {  value: 3200 }
+            ]
+            },
+            {
+            data: [
+                { title: 'Test 1', value: 6.87, color: '#8E44AD' },
+                { title: 'Test 2', value: 14.56, color: '#81CFE0' },
+                { title: 'Test 3', value: 7.82, color: '#EB974E' }
+            ]
+            },
+            {
+            data: [
+                { title: 'Test 1', value: 10 },
+                { title: 'Test 2', value: 10 },
+                { title: 'Test 3', value: 20 },
+                { title: 'Test 4', value: 5 },
+                { title: 'Test 5', value: 30 },
+                { title: 'Test 6', value: 5 },
+                { title: 'Test 7', value: 20 }
+            ]
+        }
+    ])    
   }
 
   randomization(){
